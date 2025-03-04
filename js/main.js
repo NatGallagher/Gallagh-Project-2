04302b71cc750e630fd03e8d17b0a6cda6335a1b
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-04 09:52:49 -0500

    search and clear btn added and confirmed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 function page_load() {
     console.log("giphy: page_load");
 }
+
+function searchBtn() {
+    console.log("giphy: searchBtn");
+}
+
+function clearBtn() {
+    console.log("giphy: clearBtn");
+}
