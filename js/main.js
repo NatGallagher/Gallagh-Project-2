cc3e1b680549840f45e2c9fa3243d7b8f516b929
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-04 10:00:39 -0500

    main.js update

--- js/main.js
+++ js/main.js
@@ -3,9 +3,20 @@ function page_load() {
 }
 
 function searchBtn() {
+    const divDisplayInfo = document.getElementById("div-display-info");
+    const userInput = document.getElementById("user-input");
+
+    divDisplayInfo.innerText = userInput.value;
+
     console.log("giphy: searchBtn");
 }
 
 function clearBtn() {
     console.log("giphy: clearBtn");
+
+    const divDisplayInfo = document.getElementById("div-display-info");
+    const userInput = document.getElementById("user-input");
+
+    divDisplayInfo.innerText = "";
+    userInput.value = "";
 }
