91517f9c828533bd24490d3ed0a88d3d264cb627
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-04 10:41:26 -0500

    api info added, image displaying on screen

--- js/main.js
+++ js/main.js
@@ -3,12 +3,40 @@ function page_load() {
 }
 
 function searchBtn() {
+    console.log("giphy: searchBtn");
+
     const divDisplayInfo = document.getElementById("div-display-info");
     const userInput = document.getElementById("user-input");
 
-    divDisplayInfo.innerText = userInput.value;
+    //divDisplayInfo.innerText = userInput.value;
 
-    console.log("giphy: searchBtn");
+    const searchText = "transformers";
+    const giphyApiKey = "jOxDlf3Mt8SbKWzrI7MJEUrWmhYljT5C";
+    const giphyUrl = `https://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&q=${searchText}&limit=25&rating=g`;
+
+    fetch(giphyUrl) 
+    .then(response => {
+        if(!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+
+        const giphyImage = `<img class='image-round' width='200' height='150' src='${data.data[0].images.original.url}'>`
+        divDisplayInfo.innerHTML = giphyImage;
+
+        /*const giphapi_image = `<img width='200' height='150' src='${data.data[0].images.original.url}'>`
+
+        divDisplayInfo.innerHTML = giphapi_image
+        */
+    })
+    .catch(error => {
+        console.error('## There was a problem with the fetch operation:', error);
+        divDisplayInfo.innerText = error;
+      });
+  
 }
 
 function clearBtn() {
